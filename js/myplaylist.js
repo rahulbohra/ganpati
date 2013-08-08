200ac0e5b826f435734131f08e31acd0bc7a85b3
Rahul Bohra <rahulthespaceman@gmail.com>
2013-08-08 16:54:21 +0530

    initial commit

---
+++ js/myplaylist.js
@@ -0,0 +1,31 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        title:'Sukhharta Duhkharta',
+        artist:'Amit Sharma',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.jpg',
+    },
+	{
+        mp3:'mix/1.mp3',
+        title:'Ganpati Beej Mantra',
+        artist:'Kedar Pandit, Sanjeev Abhyankar, Sharma , Rattan Mohan',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:43',
+        cover:'mix/2.jpg'
+    }
+];
